505acc1ad79d7221bb4271663bee8737b49985d3
dignifiedquire <me@dignifiedquire.com>
2022-02-17 21:41:50 +0100

    examples: add importer

---
+++ examples/importer.rs
@@ -0,0 +1,34 @@
+use std::env;
+
+use eyre::Result;
+use flatfs::Flatfs;
+use rocksfs::RocksFs;
+
+fn main() -> Result<()> {
+    let mut args = env::args();
+    let old_path = args.nth(1).unwrap();
+    let new_path = args.next().unwrap();
+
+    println!("Importing from {:?} into {:?}", old_path, new_path);
+
+    let flatfs = Flatfs::new(old_path)?;
+    let rocksfs = RocksFs::new(new_path)?;
+
+    let mut count = 0;
+    let mut size = 0;
+    for r in flatfs.iter() {
+        let (key, value) = r?;
+        count += 1;
+        size += value.len();
+
+        rocksfs.put(key, value)?;
+
+        if size % 1000 == 0 {
+            println!("{count} - {}bytes");
+        }
+    }
+
+    println!("Imported {count} values, of size {size} bytes");
+
+    Ok(())
+}
